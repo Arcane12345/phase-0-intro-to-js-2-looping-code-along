caab0604cf8c4f1d64cf9939d90e987053d49e2b
Eric <macario41@gmail.com>
2023-03-29 10:46:50 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+const Cards = [
+    "Thank you, Guadalupe, for the wonderful surprise gift!",
+    "Thank you, Ollie, for the wonderful surprise gift!",
+    "Thank you, Aki, for the wonderful surprise gift!",
+  ];
+  function writeCards() {
+    return Cards
+  }
+  
+  function countDown(){
+
+  
+  let intenger = 10
+  while (intenger > -1) {
+    console.log(intenger--);
+  }
+}
